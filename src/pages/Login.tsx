9910bd71b4625204e0efbe00fa588d0f58e758e4
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-08-04 16:11:26 -0300

    Criando componente de input

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,14 +1,25 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Image, Text, VStack, Box } from "native-base";
 import LogoIMG from '../assets/logo.png'
+
+import {Input} from '../components/FormInput'
 export function Login(){
     return(
+        <Box flex={1}  bgColor="gray.100">
             <VStack>
                 <Center>
                     <Image source={LogoIMG}/>
                     <Text fontSize="3xl" fontFamily="heading">marketspace</Text>
                     <Text fontFamily="body">Seu espaço de compra e venda</Text>
                 </Center>
+
+                <Center mt={24}>
+                    <Text mb={2}>Acesse sua conta</Text>
+                    <Input placeholder="E-mail"/>
+                    <Input placeholder="Senha"/>
+                    
+                </Center>
             </VStack>
+        </Box>
         
     )
 }
