e03c3a7e1361bb30af7cf6890e0a8c236bd2d1c3
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-08-09 16:30:52 -0300

    Header SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,12 +1,28 @@
-import { Center, Text, VStack } from "native-base";
+import { Center, Image, Text, VStack, Box } from "native-base";
+import LogoIMG from '../assets/logo.png'
 
-export function SignIn(){
+import {Input} from '../components/FormInput'
+import { Button } from "../components/Button";
+
+export function SignUp(){
     return(
-        <Center>
+        <Box flex={1}  bgColor="gray.100">
             <VStack>
+                <Center>
+                    <Image source={LogoIMG} size='xs'/>
+                    <Center>
+                        <Text fontSize="3xl" fontFamily="heading">Boas vindas!</Text>
+                        <Text fontFamily="body">Crie sua conta e use o espaço para comprar</Text>
+                        <Text fontFamily="body">itens variados e vender seus produtos</Text>
+                    </Center>
+                </Center>
+
+                
+
+
                 
             </VStack>
-        </Center>
+        </Box>
         
     )
 }
