882b2b86e6017e6269cdf63c58ecb900223b88b1
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-08-11 15:19:59 -0300

    Criando rotas publicas da aplicação

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,13 @@ import {Center, Button as NativeBaseButton} from 'native-base'
 type ButtonProps = {
     text:string
     color : 'blue.300' | 'gray.200' | 'black'
+    onPress ?: () => void;
 }
-export function Button({text, color}: ButtonProps){
+export function Button({text, color, onPress}: ButtonProps){
     return(
   
-    <NativeBaseButton w='3/4'  bgColor={color} >
-       
+    <NativeBaseButton w='3/4'  bgColor={color} onPress={onPress}>
             {text}
-    
     </NativeBaseButton>
    
     )
